cb6c1e5a8e032c3fb408bdf085ed4d806259fd3a
tsukumi <tsukumijima@users.noreply.github.com>
2022-09-09 16:53:32 +0900

    Update: ビルドのファイル名やメタデータを変更

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -25,9 +25,9 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
   FlutterWindow window(project);
-  Win32Window::Point origin(10, 10);
-  Win32Window::Size size(1280, 720);
-  if (!window.CreateAndShow(L"real_esrgan_gui", origin, size)) {
+  Win32Window::Point origin(0, 0);
+  Win32Window::Size size(0, 0);
+  if (!window.CreateAndShow(L"Real-ESRGAN-GUI", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
